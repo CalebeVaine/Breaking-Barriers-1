a1101ec3c2ea51738498518f90353648cba4d878
Jullya2 <jullya.monteiro@alunos.sc.senac.br>
2025-08-25 14:37:56 -0300

    Cena

--- Breaking Barriers 1 jogo/Assets/scripts/menu.cs
+++ Breaking Barriers 1 jogo/Assets/scripts/menu.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Nome da cena do jogo (coloque o nome exato que está no Build Settings)
+    public string cenaJogo = "GameScene";
 
-    // Update is called once per frame
-    void Update()
+    // Essa função será chamada quando você apertar o botão
+    public void Jogar()
     {
-        
+        SceneManager.LoadScene(cenaJogo);
     }
 }
