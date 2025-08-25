0210a682b6f38bf849f8009a07082163a012c2ea
jesusborrome <jesus.diaz@alunos.sc.senac.br>
2025-08-25 14:16:08 -0300

    comit

--- Breaking Barriers 1 jogo/Assets/scripts/SpawnOnTouch.cs
+++ Breaking Barriers 1 jogo/Assets/scripts/SpawnOnTouch.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class SpawnOnTouch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject objetoParaSpawnar; // O que será criado
+    public Transform pontoDeSpawn;       // Onde será criado (opcional)
 
-    // Update is called once per frame
-    void Update()
+    private bool jaSpawnou = false;      // Evita múltiplos spawns
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (!jaSpawnou && other.CompareTag("Player"))
+        {
+            // Define onde spawnar: no ponto indicado ou na posição do objeto
+            Vector3 posicaoSpawn = pontoDeSpawn != null ? pontoDeSpawn.position : transform.position;
+
+            Instantiate(objetoParaSpawnar, posicaoSpawn, Quaternion.identity);
+            jaSpawnou = true;
+        }
     }
 }
